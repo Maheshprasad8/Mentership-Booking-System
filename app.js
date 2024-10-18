c2a7d1e943707eb585ae60370ab2d378e9b669e8
Mahesh kumar <162782175+Maheshprasad8@users.noreply.github.com>
2024-10-19 01:26:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const facultyData = [
       expertise: "System Analysis and Design,Computer Graphics",
       availableSlots: ["10:00 AM", "11:00 AM", "2:00 PM"],
       profileClass: "profile-1", // Class for styling
-      image: "http://sliet.ac.in/wp-content/uploads/avatars/5/6fc17d6735998f4f22a0e91fbf43b75c-bpfull.jpg" // Placeholder image
+      image: "Birmohan.png" // Placeholder image
   },
   {
       name: "Dr. Damanpreet Singh",
@@ -14,7 +14,7 @@ const facultyData = [
       expertise: "Data Analysis, Big Data",
       availableSlots: ["9:00 AM", "1:00 PM","2:00PM"],
       profileClass: "profile-2", // Class for styling
-      image: "http://sliet.ac.in/wp-content/uploads/avatars/4/65783e7db0473-bpfull.jpg" // Placeholder image
+      image: "Damanprit.png" // Placeholder image
   },
   {
       name: "Dr. Major Singh Goraya",
@@ -22,7 +22,7 @@ const facultyData = [
       expertise: "Data Structure and algroithm",
       availableSlots: ["9:00 PM", "10:00 PM","11:30 PM"],
       profileClass: "profile-3", // Class for styling
-      image: "http://sliet.ac.in/wp-content/uploads/avatars/35/5e69c6be20c7e-bpfull.jpg" // Placeholder image
+      image: "Major.png" // Placeholder image
   },
   {
     name: "Dr. Manoj Sachan",
@@ -30,7 +30,7 @@ const facultyData = [
     expertise: "Object oriented programming",
     availableSlots: ["12:00 PM", "3:00 PM","4:00 PM"],
     profileClass: "profile-4", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/6/605c66bd3e3bc-bpfull.jpg"
+    image: "Sachan.png"
   },
   
   {
@@ -39,7 +39,7 @@ const facultyData = [
     expertise: "Cloud Computing,web develpoment",
     availableSlots: ["10:00 PM", "1:00 PM","3:00 PM"],
     profileClass: "profile-5", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/14/5f2245e7a7f1bde5dee5bda48e09d411-bpfull.jpg"
+    image: "Kaur.png"
   },
   {
     name: "Dr. Amar Nath",
@@ -47,7 +47,7 @@ const facultyData = [
     expertise: "ML,Data Science,AI",
     availableSlots: ["8:00 PM", "10:00 PM","2:00 PM"],
     profileClass: "profile-6", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/467/66b8bb350cc46-bpfull.jpg"
+    image: "Amar.png"
   },
   {
     name: "Dr. Jagdeep Singh",
@@ -55,7 +55,7 @@ const facultyData = [
     expertise: "Machine Learning,Development",
     availableSlots: ["11:00 PM", "3:00 PM","4:30 PM"],
     profileClass: "profile-7", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/466/66f81ca30f4ff-bpfull.jpg"
+    image: "Jagdip.png"
   },
   {
     name: "Dr. Jatinder Pal Singh",
@@ -63,7 +63,7 @@ const facultyData = [
     expertise: "Machine Learning,Deep Learning",
     availableSlots: ["9:30 PM", "11:30 PM","4:30 PM"],
     profileClass: "profile-8", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/344/64edd5a68d11b-bpfull.jpg"
+    image: "Jp Sir.png"
   },
   {
     name: "Dr. Tajinder Singh",
@@ -71,7 +71,7 @@ const facultyData = [
     expertise: "Computer Network,Cyber Security",
     availableSlots: ["11:00 PM", "2:00 PM","4:30 PM"],
     profileClass: "profile-9", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/468/64e3532d30fac-bpfull.jpg"
+    image: "Tejinder.png"
   },
   {
     name: "Dr. Manminder Singh",
@@ -79,7 +79,7 @@ const facultyData = [
     expertise: "Computer Architecture,Operating Systems",
     availableSlots: ["12:00 PM", "1:00 PM","2:30 PM"],
     profileClass: "profile-10", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/16/66693282b6269-bpfull.jpg"
+    image: "Manminder.png"
   },
   {
     name: "Dr. Preetpal Kaur Buttar",
@@ -87,7 +87,7 @@ const facultyData = [
     expertise: "Image Processing, Remote Sensing",
     availableSlots: ["1:00 PM", "3:00 PM","4:30 PM"],
     profileClass: "profile-11", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/349/632d98c75d80f-bpfull.jpg"
+    image: "Mam.png"
   },
   {
     name: "Dr. Utkarsh",
@@ -95,7 +95,7 @@ const facultyData = [
     expertise: "App development",
     availableSlots: ["10:00 PM", "1:30 PM","4:30 PM"],
     profileClass: "profile-12", // Class for styling
-    image: "http://sliet.ac.in/wp-content/uploads/avatars/493/6165aace595a1-bpfull.jpg"
+    image: "Utkarsh.png"
   },
 ];
 
